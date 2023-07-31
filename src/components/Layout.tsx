6ded9123b43e4c735966882b864e37e62ba67995
dani784601 <dani784601@gmail.com>
2023-07-31 12:53:03 +0900

    rename: Layout 컴포넌트 경로 변경

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from './components/NavBar'
+import NavBar from './NavBar'
 
 export default function Layout({children} : { children: React.ReactNode}) {
   return (
