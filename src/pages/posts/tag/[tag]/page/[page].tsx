c8a33ab5f4f53eeaac74e39b2baef022258ba615
dani784601 <dani784601@gmail.com>
2023-08-17 13:06:18 +0900

    test: 배포사이트에서 공백이 들어간 한글 태그 검색 불가 문제 테스트

--- src/pages/posts/tag/[tag]/page/[page].tsx
+++ src/pages/posts/tag/[tag]/page/[page].tsx
@@ -17,7 +17,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
       });
     })
   )
-  console.log('params', params)
   return {
     paths: params,
     fallback: 'blocking',
@@ -26,6 +25,8 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const allTagList = await getAllTags();
+  console.log('ctx.params?.tag',ctx.params?.tag)
+  console.log('allTagList',allTagList)
   const currentTag = ctx.params?.tag?.toString() ?? '';
   const currentPage = parseInt((ctx.params?.page?.toString() ?? '1'), 10) ;
   const numberOfPageByTag = await getNumberOfPageByTag(currentTag);
@@ -51,7 +52,9 @@ interface BlogTagListProps {
 }
 
 export default function BlogTagPageList({ allTagList, posts, currentPage, numberOfPageByTag, currentTag }: BlogTagListProps) {
-  console.log('postByTag',posts)
+  console.log('allTagList',allTagList)
+  console.log('currentTag',currentTag)
+  
   return (
     <main>
       <div className='container h-screen min-w-full px-8'>
